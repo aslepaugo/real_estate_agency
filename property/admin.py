762314421e2f5895331465525d4a561b9ea9cfbe
Aleksy <aslepaugo@gmail.com>
2022-11-02 07:47:44 +0100

    add phone numbers to admin display.

--- property/admin.py
+++ property/admin.py
@@ -5,7 +5,7 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'town_district', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'owners_phonenumber', 'owner_pure_phone']
     list_editable = ['new_building']
     list_filter = ['new_building', 'floor', 'rooms_number', 'has_balcony', 'active']
     raw_id_fields = ['liked_by']
