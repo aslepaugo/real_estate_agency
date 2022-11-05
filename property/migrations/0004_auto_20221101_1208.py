2c95b00f1d625c8e4ad3b12b69aaf269ea927da3
Aleksy <aslepaugo@gmail.com>
2022-11-05 16:36:44 +0100

    optimize update for new building.

--- property/migrations/0004_auto_20221101_1208.py
+++ property/migrations/0004_auto_20221101_1208.py
@@ -4,9 +4,9 @@ from django.db import migrations
 
 def set_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        flat.new_building = flat.construction_year >= 2015
-        flat.save()
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
+    
 
 class Migration(migrations.Migration):
 
